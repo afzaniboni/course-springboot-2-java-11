24ddd76738c5a0ff6b8f99e91f83bb7967b41801
Andre Zaniboni <afzaniboni@gmail.com>
2020-10-22 22:53:39 -0300

    Commit com as implementações de Category

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -8,8 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -23,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -32,13 +38,20 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
-		Order o4 = new Order(null, Instant.parse("2019-07-23T15:21:22Z"), u1);
-		Order o5 = new Order(null, Instant.parse("2019-07-24T15:21:22Z"), u2);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u3);
+		Order o4 = new Order(null, Instant.parse("2019-07-23T15:21:22Z"), OrderStatus.PAID, u1);
+		Order o5 = new Order(null, Instant.parse("2019-07-24T15:21:22Z"), OrderStatus.CANCELED, u2);
 
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5));
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");	
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 	}
 
 }
